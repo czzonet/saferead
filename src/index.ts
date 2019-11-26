1efa37dc6547574c2b1daadfae1a0520318db932
czz <czzonet@163.com>
2019-11-26 16:08:31 +0800

    index saferead test add||主程序加入安全读取的演示测试

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
 console.log('Here we are.');
+
+import { saferead } from './util/saferead'
+
+let s = {} as any
+let t = saferead(() => s.a.b.c.d)
+
+console.log('read property from s:s.a.b.c.d:', t)
+
+
+
+
+
+
+
+
+
+
 debugger
